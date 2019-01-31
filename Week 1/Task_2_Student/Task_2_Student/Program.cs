50782887851a6da48d6c726d9bf5d8180634bd28
tdinaraa <45028995+tdinaraa@users.noreply.github.com>
2019-01-31 23:00:15 +0600

    Lab1

--- Week 1/Task_2_Student/Task_2_Student/Program.cs
+++ Week 1/Task_2_Student/Task_2_Student/Program.cs
@@ -8,6 +8,7 @@ namespace Task_2_Student
 {
     class Student //special class for Student
     {
+        //global variables of class Student
         public string name; //name is identified as string
         public string id; //id is identified as string (it may consists letters and numbers)
         public int year; //year is identified as int (numbers only)
@@ -15,8 +16,8 @@ namespace Task_2_Student
 
         public Student(string name, string id) //constructor with two parameters
         {
-            this.name = name; //name is identifies as a name in a class
-            this.id = id; //id is identified as an id in a class
+            this.name = name; //initialize variables using pointers
+            this.id = id; 
         }
 
         public void Addyear()
@@ -24,7 +25,7 @@ namespace Task_2_Student
             this.year++; //method that used to increase the year
         }
 
-        public void PrintInfo () //function that prints all data
+        public void PrintInfo () //method that used to print all info about student
         {
             Console.WriteLine(name);
             Console.WriteLine(id);
@@ -40,7 +41,7 @@ namespace Task_2_Student
             st.year = 1; //year of student is equal to 1
             st.Addyear(); //year is increased to one by using above method
 
-            st.PrintInfo(); //all data is printed by using above function
+            st.PrintInfo(); //all data is printed by using above method
             
 
         }
---
+++ Week 1/Task_2_secondversion/Task_2_secondversion/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_secondversion
+{
+    class Student //special class for Student
+    {
+        //global variables of class Student
+        public string name; //name is identified as string
+        public string id; //id is identified as string (it may consists letters and numbers)
+        public int year; //year is identified as int (numbers only)
+
+
+        public Student(string name, string id) //constructor with two parameters
+        {
+            this.name = name; //initialize variables using pointers
+            this.id = id;
+        }
+
+        public void AddYear() //method that used to increse the year
+        {
+            this.year++;
+        }
+
+        public void ShowInfo() //method that used to access for all info about student
+        {
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+
+        public string Name() //method to access the name
+        {
+            return this.name;
+        }
+
+        public string ID() //method to access the id
+        {
+            return this.id;
+        }
+
+        public int Year() //method to access the year
+        {
+            return this.year;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, id;
+            name = Console.ReadLine();
+            id = Console.ReadLine(); //data that are used
+
+            Student st = new Student(name, id); //new object that consists the name and id of student
+           
+            for (int i = 0; i < 4; i++) //cycles used to output data 4 times
+            {
+                st.AddYear();//year is increased by 1
+                Console.WriteLine(st.Name() + " " + st.ID() + " " + st.Year()); //all data are output
+            }
+
+        }
+    }
+}
